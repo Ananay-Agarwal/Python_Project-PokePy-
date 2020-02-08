80d693017732ca53aacfe6a49cc9e11e219e9e99
Kuro-Yaksha <60287935+Kuro-Yaksha@users.noreply.github.com>
2020-02-09 03:17:37 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import pygame as pg
+import sys
+from console import *
+from sprites import *
+from os import path
+
+
+class Game:
+
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(100, 100)
+        self.load_data()
+
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
+
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
+
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_a or event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_d or event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_w or event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_s or event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+
+gameobj = Game()
+gameobj.show_start_screen()
+
+while True:
+    gameobj.new()
+    gameobj.run()
+    gameobj.show_go_screen()
